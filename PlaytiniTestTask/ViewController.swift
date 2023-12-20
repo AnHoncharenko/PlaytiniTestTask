7ba46d4686be54ce32e010236a13c0245ee82688
Anton Honcharenko <honcharenko.ay@gbksoft.com>
2023-12-20 10:50:16 +0200

    Initial Commit

---
+++ PlaytiniTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaytiniTestTask
+//
+//  Created by Anton Honcharenko on 20.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
